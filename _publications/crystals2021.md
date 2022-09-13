2f853c132929b37a7b3156810d8da05e42322bad
LoKu2811 <mail@lorenzkuger.de>
2022-09-13 14:46:20 +0200

    add publications

---
+++ _publications/crystals2021.md
@@ -0,0 +1,8 @@
+---
+layout: default
+title: Modeling and Reconstruction Strategy for Compton Scattering Tomography with Scintillation Crystals
+authors: Lorenz Kuger and Gaël Rigaud
+journal: Crystals 11(6):641
+year: 2021
+doi: http://doi.org/10.3390/cryst11060641
+---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Modeling and Reconstruction Strategy for Compton Scattering Tomography with Scintillation Crystals",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/crystals2021/",
+        "teaser": "/assets/images/teaser.png"
+      },{
+        "title": "On multiple scattering in Compton scattering tomography and its impact on fan-beam CT",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/ipi2022/",
+        "teaser": "/assets/images/teaser.png"
+      }]
